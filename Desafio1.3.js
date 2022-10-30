dde295e5d64f9482d8279f5bbd75763b443179a4
Airton Gamarra <airtongamarra575@gmail.com>
2022-10-30 10:54:53 -0300

    Repositorio

---
+++ Desafio1.3.js
@@ -0,0 +1,8 @@
+//1.3 Agora faça com que o algoritmo do exercício anterior exiba a mesma contagem mas na ordem inversa.
+//Ex.: 50 45 40 35 30 25 … 5 0 Fim!
+
+var numero = 0
+for(var contador = 50; contador >= numero; contador-- ){
+console.log(contador)
+contador = contador - 4
+}
